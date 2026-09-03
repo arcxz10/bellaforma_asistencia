06f277154dfa58b4492eb9d98a085d18fd870a60
arcxz10 <sebas.da200@gmail.com>
2026-09-03 13:11:19 -0500

    Update conexion.php

--- reporte_asistencia_grupo_bellaforma/conexion.php
+++ reporte_asistencia_grupo_bellaforma/conexion.php
@@ -1,12 +1,11 @@
 <?php
-$host = getenv('MYSQLHOST') ?: 'localhost';
-$user = getenv('MYSQLUSER') ?: 'root';
-$pass = getenv('MYSQLPASSWORD') ?: '';
-$db   = getenv('MYSQLDATABASE') ?: 'railway';
-$port = getenv('MYSQLPORT') ?: 3306;
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "zfhJTFzwzIdzxFGOrQXoHAIsbGURuxtC";
+$db   = "railway";
+$port = 3306;
 
-// Es importante pasar el puerto y usar la IP 127.0.0.1 si fuera local, pero con getenv se conecta por red a Railway
-$conexion = new mysqli($host, $user, $pass, $db, (int)$port);
+$conexion = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
