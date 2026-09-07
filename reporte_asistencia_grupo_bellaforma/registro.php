842667d620a00c1dfc7c059287184c57cb5dcdc1
arcxz10 <sebas.da200@gmail.com>
2026-09-07 11:45:23 -0500

    Update registro.php

--- reporte_asistencia_grupo_bellaforma/registro.php
+++ reporte_asistencia_grupo_bellaforma/registro.php
@@ -1,5 +1,7 @@
 date_default_timezone_set("America/Bogota");
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once "conexion.php";
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
