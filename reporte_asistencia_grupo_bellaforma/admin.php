acd3f84870b4febeec666dc550c01cfeaf2be53d
arcxz10 <sebas.da200@gmail.com>
2026-09-18 09:19:30 -0500

    Update admin.php

--- reporte_asistencia_grupo_bellaforma/admin.php
+++ reporte_asistencia_grupo_bellaforma/admin.php
@@ -52,7 +52,7 @@ function formatoHora($hora)
     $partes = explode(":", $hora);
 
     $horas = (int) ($partes[0] ?? 0);
-    $minutos = (int) ($partes ?? 0);
+    $minutos = (int) ($partes ?? 0); 
 
     $periodo = $horas >= 12 ? "PM" : "AM";
 
