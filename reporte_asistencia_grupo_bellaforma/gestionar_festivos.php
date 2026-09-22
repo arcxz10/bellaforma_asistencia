b8db2d486c6dc60717108b63ab25737165aecb08
arcxz10 <sebas.da200@gmail.com>
2026-09-22 14:25:40 -0500

    Update gestionar_festivos.php

--- reporte_asistencia_grupo_bellaforma/gestionar_festivos.php
+++ reporte_asistencia_grupo_bellaforma/gestionar_festivos.php
@@ -29,6 +29,10 @@ $conexion->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionar Festivos | Grupo Bellaforma</title>
+
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
     <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="css/festivos.css">
 
