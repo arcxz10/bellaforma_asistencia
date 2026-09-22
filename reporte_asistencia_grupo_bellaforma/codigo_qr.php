e1c23b6245fbc7880afd10c6b74f88ef96530101
arcxz10 <sebas.da200@gmail.com>
2026-09-22 14:23:52 -0500

    Update codigo_qr.php

--- reporte_asistencia_grupo_bellaforma/codigo_qr.php
+++ reporte_asistencia_grupo_bellaforma/codigo_qr.php
@@ -12,6 +12,10 @@ if (!isset($_SESSION["admin_id"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Código QR | Grupo Bellaforma</title>
+
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
     <style>
         * {
