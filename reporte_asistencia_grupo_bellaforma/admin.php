ac8620bd0d2220122eb6fdc47e5e5d940923526d
arcxz10 <sebas.da200@gmail.com>
2026-09-22 14:23:27 -0500

    Update admin.php

--- reporte_asistencia_grupo_bellaforma/admin.php
+++ reporte_asistencia_grupo_bellaforma/admin.php
@@ -1023,6 +1023,10 @@ $resultadoEmpleados =
         Panel Administrativo - Bellaforma
     </title>
 
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
     <link
         rel="stylesheet"
         href="css/admin.css"
