ed28a59586126eb4f30d4dbd164e5e940c30aca4
arcxz10 <sebas.da200@gmail.com>
2026-09-22 14:28:00 -0500

    Update registro_inicial.php

--- reporte_asistencia_grupo_bellaforma/registro_inicial.php
+++ reporte_asistencia_grupo_bellaforma/registro_inicial.php
@@ -98,6 +98,10 @@ $justificacionSalida = trim($_POST['justificacion_salida'] ?? '');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Asistencia | Grupo Bellaforma</title>
+
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
     <link rel="stylesheet" href="css/registro_inicial.css">
 </head>
 <body>
