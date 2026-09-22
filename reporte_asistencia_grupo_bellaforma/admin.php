4f8cac5609cdf372710a472c8d38c6e7782a682c
arcxz10 <sebas.da200@gmail.com>
2026-09-21 20:17:43 -0500

    Update admin.php

--- reporte_asistencia_grupo_bellaforma/admin.php
+++ reporte_asistencia_grupo_bellaforma/admin.php
@@ -1032,8 +1032,10 @@ $resultadoEmpleados =
 
         <div class="sidebar-header">
 
+            <img src="img/logo-blanco.png" alt="Grupo Bella Forma S.A.S." class="logo-marca-sidebar">
+
             <h2>
-                🏢 BELLAFORMA
+                GRUPO BELLA FORMA
             </h2>
 
             <p>
