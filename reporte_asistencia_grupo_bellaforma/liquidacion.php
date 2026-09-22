10c3029a78011ada8fa481d5d98dc3bb5c89a530
arcxz10 <sebas.da200@gmail.com>
2026-09-22 14:27:04 -0500

    Update liquidacion.php

--- reporte_asistencia_grupo_bellaforma/liquidacion.php
+++ reporte_asistencia_grupo_bellaforma/liquidacion.php
@@ -171,6 +171,10 @@ if ($stmt) {
         Liquidación de Nómina - Bellaforma
     </title>
 
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
     <link rel="stylesheet" href="css/admin.css">
 
     <style>
